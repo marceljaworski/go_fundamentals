e1dd694796a0d0af3d5a35dd72aa88e1408b0758
Marcelearn <jaworskimarcel@gmail.com>
2023-07-24 15:12:00 +0200

    slices with Go

---
+++ slices.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	/* Define an array containing programming languages */
+	languages := [9]string{
+		"C", "Lisp", "C++", "Java", "Python",
+		"JavaScript", "Ruby", "Go", "Rust", // Must include the trailing comma
+	}
+
+	/* Define slices */
+	classics := languages[0:3]  // alternatively languages[:3]
+	modern := make([]string, 4) // len(modern) = 4
+	modern = languages[3:7]     // include 3 exclude 7
+	new := languages[7:9]       // alternatively languages[7:]
+
+	fmt.Printf("classic languagues: %v\n", classics) // classic languagues: [C Lisp C++]
+	fmt.Printf("modern languages: %v\n", modern)     // modern languages: [Java Python JavaScript Ruby]
+	fmt.Printf("new languages: %v\n", new)           // new languages: [Go Rust]
+}
