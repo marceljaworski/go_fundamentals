20e423e3148563150dd844265d4d5863496516ad
Marcelearn <jaworskimarcel@gmail.com>
2023-07-24 15:30:09 +0200

    output key-value pair

---
+++ maps.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	/* Define a map containing the release year of several languages */
+	firstReleases := map[string]int{
+		"C": 1972, "C++": 1985, "Java": 1996,
+		"Python": 1991, "JavaScript": 1996, "Go": 2012,
+	}
+
+	/* Loop through each entry and output the name and release year */
+	for k, v := range firstReleases {
+		fmt.Printf("%s was first released in %d\n", k, v)
+	}
+}
