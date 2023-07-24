07b543d8909b97d4fdb76cdbf258f1b624580235
Marcelearn <jaworskimarcel@gmail.com>
2023-07-24 14:34:13 +0200

    variables_refactored

---
+++ variables_refactored.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 42            // Initialize and assign to a single variable
+	b, c := true, 32.0 // Initialize and assign to multiple variables
+	d := "string"
+	fmt.Println(a, b, c, d) // 42 true 32 string
+}
