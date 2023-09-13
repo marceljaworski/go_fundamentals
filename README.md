bb0d708409d2940917013586d72f09746dc6ebb6
Marcel Jaworski <marcel.jaworski@ils-gmbh.net>
2023-09-13 13:10:06 +0200

    some test

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ## create packages
 
+`go mod init packages` or go mod init `github.com/marceljaworski/packages`
 `go work init ./packages`
 
 `go mod init pointers`
--- packages/packages.go
+++ packages/packages.go
@@ -1,10 +1,12 @@
 package main
 
+// go mod init packages or github.com/marceljaworski/packages
 import "packages/package1"
 
 func main() {
 	package1.Test()
 	x := package1.TestType{}
 	x.DoOnce()
+	// x.b = false
 	x.DoOnce()
 }
